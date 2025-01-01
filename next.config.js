26851c88f991e9f5308b85967ab96ee258223265
Eren Mollaoğlu <eren.mollaoglu@outlook.com>
2025-01-01 14:36:17 +0300

    inital commit

--- next.config.js
+++ next.config.js
@@ -3,18 +3,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
-      },
-      // Eğer başka kaynaklardan da resim kullanıyorsanız
-      // buraya ekleyebilirsiniz
-    ],
   },
   basePath: '/Portfolio',
+  assetPrefix: '/Portfolio/',
 }
 
 module.exports = nextConfig 
